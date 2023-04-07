ba953e4dbb835bb0d70a4aec9e3c2e0f0c501b6f
Joshua J Piluden <joshuajpiluden@Gmail.com>
2023-04-08 00:57:19 +0800

    Added new unit test.

--- tests/Obaki.DataChecker.Tests/Tests/Sync/XmlDataChecker_DeserializeInputString.cs
+++ tests/Obaki.DataChecker.Tests/Tests/Sync/XmlDataChecker_DeserializeInputString.cs
@@ -14,6 +14,7 @@ namespace Obaki.DataChecker.Tests.Sync
         {
             _xmlDataChecker = new XmlDataChecker(); 
         }
+
         [Fact]
         public void DeserializeInputString_ValidXmlInput_ShouldNotBeNull()
         {
@@ -34,6 +35,29 @@ namespace Obaki.DataChecker.Tests.Sync
             //Assert
             Assert.NotNull(dummyObject);
             Assert.Equal(dummyObject.Order[0].Customer, "John Doe");
+        }
+
+
+        [Fact]
+        public void DeserializeInputString_InValidXmlInput_ShouldNotBeNullAndInputIsSanitized()
+        {
+            //Arrange
+            string xmlInput = @"<Orders>
+                                <Order OrderId=""1"" Customer=""John & % Doe"">
+                                    <OrderItem ItemId=""101"" Description=""Widget"" Quantity=""3"" Price=""10.00"" />
+                                    <OrderItem ItemId=""102"" Description=""Gadget"" Quantity=""2"" Price=""15.00"" />
+                                </Order>
+                                <Order OrderId=""2"" Customer=""Jane Smith"">
+                                    <OrderItem ItemId=""201"" Description=""Thingamabob"" Quantity=""1"" Price=""25.00"" />
+                                </Order>
+                            </Orders>";
+
+            //Act
+            var dummyObject = _xmlDataChecker.DeserializeInputString<Orders>(xmlInput);
+
+            //Assert
+            Assert.NotNull(dummyObject);
+            Assert.Equal(dummyObject.Order[0].Customer, "John   Doe");
         }
     }
 }
